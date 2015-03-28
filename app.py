1be733c357bc01499a3f59ad0820fe3e6cc92c77
Shubham SHARMA <shubham@sharma.fr>
2015-03-28 11:18:44 +0100

    app2

--- app.py
+++ app.py
@@ -1,11 +1,25 @@
 import os
-from flask import Flask
+from flask import Flask, request
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello from Python!"
+    return "Hello from Parse API Server!"
+
+@app.route("/inbound/", methods=['GET', 'POST'])
+def inbound():
+    if request.method == 'POST':
+        parse()
+    else:
+        listingInbounds()
+
+def listingInbounds():
+    return "list of inbounds"
+
+def parse():
+    return request.form
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
+    app.debug = True
     app.run(host='0.0.0.0', port=port)
