1a12c4de2c66c3519ff19099997db3aa5578c9a3
Shubham SHARMA <shubham@sharma.fr>
2015-03-28 15:23:18 +0100

    frebase get display begin

--- app.py
+++ app.py
@@ -1,28 +1,32 @@
 import os
 import pprint
 import time
+import json
 from flask import Flask, request, render_template
 from firebase import firebase
 firebase = firebase.FirebaseApplication('https://parseapi.firebaseio.com/', None)
 app = Flask(__name__)
 
+def dictolist(dico):
+    l = []
+    for key in dico:
+        l.append(dico[key]);
+    return l
+
 @app.route("/")
 def hello():
     return render_template('index.html',
-                           title='Home',
-                           requestData=request,
-                           mainText='Here is just the home page',
-                           data=request.form)
+                           title='Home')
 
 @app.route("/inbound/", methods=['GET', 'POST'])
 def inbound():
     if request.method == 'POST':
-        data = parse(request.form)
+        data = parse(json.loads(request.data))
         return render_template('index.html',
                                 title='Inbound',
                                 requestData=request,
                                 mainText='Got it !',
-                                data=request.form)
+                                data=data)
     else:
         data = listingInbounds()
         return render_template('list.html',
@@ -31,14 +35,15 @@ def inbound():
                                 data=data)
 
 def listingInbounds():
-    data = firebase.get('/mails', None)
-    return data
+    dico = firebase.get('/mails', None)
+    l = dictolist(dico)
+    return l
 
 def parse(data):
-    pusableData = data.copy()
-    pusableData.add('time', time.strftime("%c"))
+    pusableData = data
+    #pusableData.add('time', time.strftime("%c"))
     snapshot = firebase.post('/mails', pusableData)
-    pprint.pprint(pusableData)
+    return pusableData
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
