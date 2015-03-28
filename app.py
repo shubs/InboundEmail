d1fe2b0885ea5359f564cf79ce3e43b35d4566c2
Shubham SHARMA <shubham@sharma.fr>
2015-03-28 12:45:04 +0100

    all good but listing todo

--- app.py
+++ app.py
@@ -1,24 +1,44 @@
 import os
-from flask import Flask, request
+import pprint
+import time
+from flask import Flask, request, render_template
+from firebase import firebase
+firebase = firebase.FirebaseApplication('https://parseapi.firebaseio.com/', None)
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello from Parse API Server!"
+    return render_template('index.html',
+                           title='Home',
+                           requestData=request,
+                           mainText='Here is just the home page',
+                           data=request.form)
 
 @app.route("/inbound/", methods=['GET', 'POST'])
 def inbound():
     if request.method == 'POST':
-        parse()
+        data = parse(request.form)
+        return render_template('index.html',
+                                title='Inbound',
+                                requestData=request,
+                                mainText='Got it !',
+                                data=request.form)
     else:
-        listingInbounds()
+        data = listingInbounds()
+        return render_template('list.html',
+                                title='Inbound',
+                                mainText='list of mail recieved on this url',
+                                data=data)
 
 def listingInbounds():
-    return "list of inbounds"
+    data = firebase.get('/mails', None)
+    return data
 
-def parse():
-    dump(request)
-    return "thx"
+def parse(data):
+    pusableData = data.copy()
+    pusableData.add('time', time.strftime("%c"))
+    snapshot = firebase.post('/mails', pusableData)
+    pprint.pprint(pusableData)
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
